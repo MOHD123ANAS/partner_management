0d5790e15a4c52a10412ed5a6abfe180568227f6
MOHD123ANAS <mohammedanas18025@gmail.com>
2025-10-24 15:28:41 +0530

    po update

--- partner_management/customization/purchase.py
+++ partner_management/customization/purchase.py
@@ -3,43 +3,68 @@ from frappe.utils import nowdate
 
 @frappe.whitelist()
 def create_po_and_pi_for_supplier(doc, method):
-
     supplier_items = {}
+
     
-    # Collect items grouped by supplier
     for item in doc.items:
         if item.delivered_by_supplier and item.supplier:
-            if item.supplier not in supplier_items:
-                supplier_items[item.supplier] = []
-            supplier_items[item.supplier].append(item)
-    
+            supplier_items.setdefault(item.supplier, []).append(item)
+
     for supplier, items in supplier_items.items():
-        # Create Purchase Order
+
+        
+        supplier_doc = frappe.get_doc("Supplier", supplier)
+        price_list = supplier_doc.default_price_list
+
+        
+        if not price_list:
+            frappe.throw(f"Supplier {supplier} does not have a Default Buying Price List set in Supplier Master.")
+
+        
         po = frappe.get_doc({
             "doctype": "Purchase Order",
             "supplier": supplier,
             "transaction_date": nowdate(),
             "schedule_date": nowdate(),
+            "buying_price_list": price_list,
             "items": []
         })
 
         for so_item in items:
+            
+            rate = frappe.db.get_value(
+                "Item Price",
+                {
+                    "item_code": so_item.item_code,
+                    "price_list": price_list
+                },
+                "price_list_rate"
+            )
+
+            
+            if rate is None:
+                frappe.throw(
+                    f"No rate found for Item {so_item.item_code} "
+                    f"in Price List {price_list} (Supplier: {supplier})."
+                )
+
             po.append("items", {
                 "item_code": so_item.item_code,
                 "qty": so_item.qty,
-                "rate": so_item.rate,  
+                "rate": rate,
                 "sales_order": doc.name,
                 "so_detail": so_item.name
             })
-        
+
         po.insert(ignore_permissions=True)
         po.submit()
-        
+
         
         pi = frappe.get_doc({
             "doctype": "Purchase Invoice",
             "supplier": supplier,
             "posting_date": nowdate(),
+            "buying_price_list": price_list,
             "items": []
         })
 
@@ -51,6 +76,6 @@ def create_po_and_pi_for_supplier(doc, method):
                 "purchase_order": po.name,
                 "po_detail": po_item.name
             })
-        
+
         pi.insert(ignore_permissions=True)
         pi.submit()
--- partner_management/partner_management/doctype/partners_list/partners_list.py
+++ partner_management/partner_management/doctype/partners_list/partners_list.py
@@ -1,27 +1,25 @@
-# Copyright (c) 2025, winspire tech  and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
 
 class PartnersList(Document):
     def before_submit(self):
+        
         if self.status not in ["Approved", "Rejected"]:
             frappe.throw("You can only submit if the status is 'Approved' or 'Rejected'.")
+
         if self.status == "Approved":
             if not self.partner_name:
-                frappe.throw("Enter Partner Name")
+                frappe.throw("Enter Partner Name.")
             if not self.contact_number:
-                frappe.throw("Enter Contact Number")
+                frappe.throw("Enter Contact Number.")
             if not self.contact_email_id:
-                frappe.throw("Enter Email")
-            if not self.gst_detailspdf_format: 
-                frappe.throw('Attach GST File')
+                frappe.throw("Enter Email.")
+            if not cint(self.gst_not_applicable) and not self.gst_detailspdf_format:
+                frappe.throw("Attach GST File before submitting.")
 
     def on_submit(self):
         if self.status == "Approved":
-            
-            
             partner_doc = frappe.new_doc("Partner Details")
             partner_doc.business_name = self.partner_business_name
             partner_doc.partner_name = self.partner_name
@@ -40,6 +38,3 @@ class PartnersList(Document):
 
             partner_doc.posting_date = frappe.utils.nowdate()
             partner_doc.insert(ignore_permissions=True)
-
-            
-
